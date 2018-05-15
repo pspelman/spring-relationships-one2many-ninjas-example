14355e50510898e17dcefe8cc58dfb154494e031
pspelman <phil_spelman@hotmail.com>
2018-05-15 08:02:02 -0700

    New project. Added application.properties and the pom.xml dependencies.

---
+++ src/main/java/com/philspelman/springrelationshipsninjaso2m/SpringRelationshipsNinjasO2mApplication.java
@@ -0,0 +1,12 @@
+package com.philspelman.springrelationshipsninjaso2m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRelationshipsNinjasO2mApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRelationshipsNinjasO2mApplication.class, args);
+    }
+}
